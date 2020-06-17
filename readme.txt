1ef358c131e9ca1de0e8eea868c946ad7a0d6bd6
ben <benell@bluewin.ch>
2020-06-17 10:50:02 +0200

    push

---
+++ readme.txt
@@ -0,0 +1,19 @@
+
+
+https://web-push-codelab.glitch.me/
+
+
+
+ng build --prod
+http-server -p 8080 -c-1 dist/<project-name>
+
+ng build --prod
+http-server -p 8080 -c-1 dist/my-pwa
+
+
+
+https://entwickler.de/online/javascript/progressive-web-apps-angular-579882090.html
+
+cd dist
+cd anwendungs-name
+http-server -o
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-pwa';
+
+ constructor(private swPush: SwPush) {}
+
+  async registerForPush() {
+    const subscription = await this.swPush.requestSubscription({
+       serverPublicKey:  'BCst2kw534_EurmkeZVO_u5EH8kk-FwQNNUHZqlmM9HPnATqIGgmhN1KYmSRTVbQHvqJfkgPuMNnOKUy20pdXd0',
+    });
+
+    console.log(subscription);
+  }
 }
